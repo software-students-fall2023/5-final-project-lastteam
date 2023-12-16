2486bf7da08f33497fcae38cf9d85047b3486711
Steven Liu <khl446@nyu.edu>
2023-12-15 20:40:42 -0500

    implemented findUser, deleteUser, get session, and update users. Also modified the link between user and sessions

--- webApp/model.py
+++ webApp/model.py
@@ -27,5 +27,29 @@ def create_session(user_id, date, buyIn, cashOut, highlights, location):
     }
     return db.sessions.insert_one(session).inserted_id
 
+# Function to get sessions for a specific user
 
-#[TO-DO, impelemnt findUser, deleteUser, updateUser]
+#we query the user_id to retrieve the specific session(s) for the user instead of updating it each time
+def get_sessions_for_user(user_id):
+    """
+    Retrieve all sessions for a given user.
+
+    :param user_id: The ObjectId of the user.
+    :return: List of session documents.
+    """
+    return list(db.sessions.find({"user": ObjectId(user_id)}))
+
+# Find User
+def find_user(username):
+    return db.poker_users.find_one({"username": username})
+
+# Delete User
+def delete_user(user_id):
+    db.poker_users.delete_one({"_id": ObjectId(user_id)})
+    # Optionally, delete related sessions
+    db.sessions.delete_many({"user": ObjectId(user_id)})
+
+# Update User
+def update_user(user_id, update_data):
+    db.poker_users.update_one({"_id": ObjectId(user_id)}, {"$set": update_data})
+
